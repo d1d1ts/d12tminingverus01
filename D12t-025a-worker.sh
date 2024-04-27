5d237585ea8381a4f03af82e71b884da6147948d
didit supriyanto <86543323+d1d1ts@users.noreply.github.com>
2024-04-27 18:36:01 +0700

    Update and rename D12t-025-worker to D12t-025a-worker.sh

---
+++ D12t-025a-worker.sh
@@ -0,0 +1,2 @@
+
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u RRjEiP8Zb9zepRmr9qvmFZjLmeL9apxC2t -p c=D12t_025a -t 2
